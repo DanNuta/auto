0a89b701796873be3961fe9da10c559dc4d71f9c
Dan <dany.condurari@gmail.com>
2022-09-10 10:27:10 +0300

    add

---
+++ index.js/index.js
@@ -0,0 +1,15 @@
+"use strict";
+class CreateAuto {
+    constructor(id, model, anul, pret, km, title) {
+        this.id = id,
+            this.model = model,
+            this.anul = anul,
+            this.pret = pret,
+            this.km = km,
+            this.title = title;
+    }
+}
+class AddAutoList {
+}
+const auto = new AddAutoList();
+console.log(auto);
